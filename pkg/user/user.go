7fb08480bf29605b157c88ed71d7169af061085d
xhino <xhino@brainplatform.it>
2022-12-28 11:56:05 +0100

    finished handler

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -43,9 +43,25 @@ func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*
 	if err != nil {
 		return nil, errors.New(ErrorFailedToUnmarshalRecord)
 	}
+
+	return item, nil
 }
 
-func FetchUsers() {}
+func FetchUsers(tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*[]User, error) {
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynaClient.Scan(input)
+
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	item := new([]User)
+	err = dynamodbattribute.UnmarshalMap(result.Items, item)
+	return item, nil
+}
 
 func CreateUser() {}
 
